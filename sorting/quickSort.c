f2ecb64e0f91608b30abf997ce300d309a722bc8
iambibek-1 <iambibek100@gmail.com>
2024-12-26 18:35:55 +0545

    quick sort added

--- sorting/quickSort.c
+++ sorting/quickSort.c
@@ -1,59 +1,50 @@
 #include <stdio.h>
 
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-int partition(int arr[], int low, int high)
-{
+int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
 
-    for (int j = low; j < high; j++)
-    {
-        if (arr[j] < pivot)
-        {
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
             i++;
-            swap(&arr[i], &arr[j]);
+            swap(arr, i, j);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
+    swap(arr, i + 1, high);
     return i + 1;
 }
 
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-void printArray(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-int main()
-{
+int main() {
     int arr[] = {8, 3, 2, 12, 6, 4};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("the given array is: ");
+    printf("The given array is: ");
     printArray(arr, size);
 
     quickSort(arr, 0, size - 1);
 
-    printf("the sorted array is: ");
+    printf("The sorted array is: ");
     printArray(arr, size);
 
     return 0;
